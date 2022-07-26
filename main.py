e218a66881616af92289ab9c78ed2a3aecb9afab
Sathwik Doddi <sathwik.doddi@gmail.com>
2022-07-26 00:00:56 -0400

    completed gui without ai >> two player game

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from pipes import Template
 import numpy as np
 import random
 import sys
 import pygame
-import math
+import keyboard
 
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 200)
@@ -11,7 +12,9 @@ YELLOW = (255, 255, 0)
 
 ROWS = 6
 COLUMNS = 7
+
 SQUARE = 100
+PIECERADIUS = 45
 
 def create_board():
     board = np.zeros((ROWS, COLUMNS))
@@ -58,9 +61,17 @@ def winning_move(board, piece):
                 return True
 
 def draw_board(board):
+    temp = np.flip(board, 0)
     for c in range(COLUMNS):
         for r in range(ROWS):
             pygame.draw.rect(screen, BLUE, (c*SQUARE, r*SQUARE, SQUARE, SQUARE))
+            if temp[r][c] == 0:
+                pygame.draw.circle(screen, BLACK, (c*SQUARE + 50, r*SQUARE + 50), PIECERADIUS)
+            elif temp[r][c] == 1:
+                pygame.draw.circle(screen, RED, (c*SQUARE + 50, r*SQUARE + 50), PIECERADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (c*SQUARE + 50, r*SQUARE + 50), PIECERADIUS)
+    pygame.display.update()
 
 
 board = create_board()
@@ -77,35 +88,51 @@ screen = pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
 
+myfont = pygame.font.Font('freesansbold.ttf', 75)
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
+            # Player 1 Input
+            if turn == 0:
+                x_position = event.pos[0]
+                column = x_position // 100
 
+                if is_valid_placement(board, column):
+                    row = get_next_available_row(board, column)
+                    drop_piece(board, row, column, 1)
 
-            # # Player 1 Input
-            # if turn == 0:
-            #     column = int(input("Player 1, choose your column (0, 6): "))
-            #     if (is_valid_placement(board, column)):
-            #         row = get_next_available_row(board, column)
-            #         drop_piece(board, row, column, 1)
-            #         if winning_move(board, 1):
-            #             print("Player 1 Wins! Congratulations.")
-            #             game_over = True
-
-            # # Player 2 Input
-            # else:
-            #     column = int(input("Player 2, choose your column (0, 6): "))
-            #     if (is_valid_placement(board, column)):
-            #         row = get_next_available_row(board, column)
-            #         drop_piece(board, row, column, 2)
-            #         if winning_move(board, 2):
-            #             print("Player 2 Wins! Congratulations.")
-            #             game_over = True
+                    if winning_move(board, 1):
+                        game_over = True
+                        label = myfont.render("Player 1 Wins!", 1, RED)
+                        screen.fill(BLACK)
+                        pygame.display.update()
+                        screen.blit(label, (100, 270))
+                        pygame.display.update()
             
-            # # Resetting
-            # print_board(board)
-            # if (turn == 0): turn = 1
-            # else: turn = 0
+            # Player 2 Input
+            else:
+                x_position = event.pos[0]
+                column = x_position // 100
+
+                if is_valid_placement(board, column):
+                    row = get_next_available_row(board, column)
+                    drop_piece(board, row, column, 2)
+
+                    if winning_move(board, 2):
+                        game_over = True
+                        label = myfont.render("Player 2 Wins!", 1, YELLOW)
+                        screen.fill(BLACK)
+                        pygame.display.update()
+                        screen.blit(label, (100, 270))
+                        pygame.display.update()
+
+            if not game_over: draw_board(board)
+
+            if turn == 0: turn = 1
+            else: turn = 0
+
+            if game_over:
+                keyboard.wait("esc")
